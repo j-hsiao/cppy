6cdb999fc2d6734b967852933f1d106ba4e81005
Jason Hsiao <oaishnosaj@gmail.com>
2025-07-19 21:21:49 +0800

    add check basic test return value.

--- test/tst/t.py
+++ test/tst/t.py
@@ -1,12 +1,13 @@
 from tst import testmodule
 
 import sys
+errored = False
 
 tup = ([slice(10,30)],(3.14, 101), "hello")
 counts1 = [sys.getrefcount(thing) for thing in tup]
 tupcount1 = sys.getrefcount(tup)
 print('arguments:', tup)
-testmodule.basic_test(*tup)
+errored = not testmodule.basic_test(*tup)
 counts2 = [sys.getrefcount(thing) for thing in tup]
 if counts1 != counts2:
     print('-------------------------')
@@ -14,6 +15,7 @@ if counts1 != counts2:
     print('-------------------------')
     print('before', counts1)
     print('after ', counts2)
+    errored = True
 tupcount2 = sys.getrefcount(tup)
 if tupcount1 != tupcount2:
     print('-------------------------')
@@ -21,6 +23,8 @@ if tupcount1 != tupcount2:
     print('-------------------------')
     print('before', tupcount1)
     print('after ', tupcount2)
+    errored = True
+
 
 
 
@@ -29,3 +33,5 @@ if tupcount1 != tupcount2:
 
 # print('tup creation:', 'pass' if testmodule.tupassign_test() == (1,'hello world!',1.5,3.25,5, ('hello', 42)) else 'fail')
 # print(testmodule.tupassign_test())
+if errored:
+    sys.exit(1)
