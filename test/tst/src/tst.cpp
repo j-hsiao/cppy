82302588fd05b9e9a38e4387cbfa2a03b9ecf594
Jason Hsiao <oaishnosaj@gmail.com>
2025-07-30 23:25:39 +0800

    Update tests.

--- test/tst/src/tst.cpp
+++ test/tst/src/tst.cpp
@@ -8,15 +8,13 @@
 //#include <cppy/util.hpp>
 
 #include <iostream>
-
-PyObject* basic_test(PyObject *m, PyObject *args_)
-{
+PyObject* test_convert(PyObject *m, PyObject *args_) {
 	{
 		cppy::Object<float> floatfromdouble(3.14);
 		cppy::Object<float> floatfromfloat(32.4f);
 		cppy::Object<float> floatfromint(3);
 #		define CHECK(...) \
-		std::cerr << #__VA_ARGS__ ": "; \
+		std::cerr << "    " #__VA_ARGS__ ": "; \
 		if (!(__VA_ARGS__)) { \
 			std::cerr << "fail" << std::endl; \
 			Py_RETURN_FALSE; \
@@ -30,121 +28,71 @@ PyObject* basic_test(PyObject *m, PyObject *args_)
 		CHECK(32.4f == floatfromfloat)
 		CHECK(3 == floatfromint)
 	}
-
 	{
-		std::cout << "------------------------------" << std::endl
-		          << "Running basic test as generic borrowed reference object." << std::endl;
-		cppy::Object<> args(args_);
-		std::cout << "  size: " << args.size() << std::endl;
-		std::cout << "  str : " << args.str() << std::endl;
-		std::cout << "  repr: " << args.repr() << std::endl;
+		cppy::Object<int> intfromdouble(3.14);
+		cppy::Object<int> intfromfloat(4.13f);
+		cppy::Object<int> intfromint(2);
+
+		CHECK(intfromdouble.to<double>() == 3.0)
+		CHECK(intfromfloat.to<double>() == 4.0)
+		CHECK(intfromint.to<double>() == 2.0)
+		CHECK(intfromdouble.to<int>() == 3)
+		CHECK(intfromfloat.to<int>() == 4)
+		CHECK(intfromint.to<int>() == 2)
+		CHECK(3 == intfromdouble)
+		CHECK(4 == intfromfloat)
+		CHECK(2 == intfromint)
+		CHECK(3.0 == intfromdouble)
+		CHECK(4.0 == intfromfloat)
+		CHECK(2.0 == intfromint)
 	}
+	Py_RETURN_TRUE;
+}
 
+PyObject* test_list(PyObject *m, PyObject *args_) {
 	{
-		std::cout << "------------------------------" << std::endl
-		          << "Running basic test as generic owned reference object." << std::endl;
-		cppy::Object<> borrowed(args_);
-		cppy::Object<PyObject> owned(borrowed);
-		std::cout << "  size: " << owned.size() << std::endl;
-		std::cout << "  str : " << owned.str() << std::endl;
-		std::cout << "  repr: " << owned.repr() << std::endl;
-	}
-
-	{
-		std::cout << "------------------------------" << std::endl
-		          << "indexing const generic borrowed reference." << std::endl;
-		const cppy::Object<> args(args_);
-		for (int i=0; i<args.size(); ++i)
-		{ std::cout << "    " << i << ": " << args[i]->str() << std::endl; }
-	}
-
-
-	{
-		std::cout << "------------------------------" << std::endl
-		          << "indexing non-const generic borrowed reference." << std::endl;
-		cppy::Object<> args(args_);
-		for (int i=0; i<args.size(); ++i)
-		{ std::cout << "    " << i << ": " << args[i]->str() << std::endl; }
-	}
-
-	{
-		std::cout << "------------------------------" << std::endl
-		          << "using a borrowed Tuple" << std::endl;
-		cppy::TupleRef args(args_);
-		for (int i=0; i<args.size(); ++i)
-		{ std::cout << "    " << i << ": " << args[i]->str() << std::endl; }
-	}
-
-	{
-		std::cout << "------------------------------" << std::endl
-		          << "testing tuple creation" << std::endl;
-		cppy::TupleRef args(args_);
-		if (args.size() >= 3) {
-			cppy::Tuple tmp(
-				args[0]->obj,
-				args[1]->obj,
-				args[2]->obj
-			);
-			std::cout << tmp.str() << std::endl;
-		}
-		else {
-			cppy::Tuple tmp(1, 2, 3);
-			std::cout << tmp.str() << std::endl;
-		}
-	}
-
-	{
-		std::cout << "------------------------------" << std::endl
-		          << "tuple size_+slice" << std::endl;
-		cppy::TupleRef args(args_);
-		if (args.check()) {
-			std::cout << args.slice(1,args.size_()).str() << std::endl;
-		}
-	}
-
-	{
-		std::cout << "------------------------------" << std::endl
-		          << "list creation" << std::endl;
-		std::cout << "list" << std::endl;
+		std::cout << "    ------------------------------" << std::endl
+		          << "    list creation" << std::endl;
+		std::cout << "    list" << std::endl;
 		cppy::List lst("hello", 72);
 		if (lst.str().string() != "['hello', 72]") { Py_RETURN_FALSE; }
-		std::cout << "------------------------------" << std::endl
-		          << "list to tuple" << std::endl;
+		std::cout << "    ------------------------------" << std::endl
+		          << "    list to tuple" << std::endl;
 		if (lst.tuple().str().string() != "('hello', 72)") { Py_RETURN_FALSE; }
-		std::cout << "------------------------------" << std::endl
-		          << "extend" << std::endl;
+		std::cout << "    ------------------------------" << std::endl
+		          << "    extend" << std::endl;
 		lst.extend(lst.tuple());
 		if (lst.str().string() != "['hello', 72, 'hello', 72]") { Py_RETURN_FALSE; }
-		std::cout << "------------------------------" << std::endl
-		          << "clear" << std::endl;
+		std::cout << "    ------------------------------" << std::endl
+		          << "    clear" << std::endl;
 		lst.clear();
 		if (lst.str().string() != "[]") { Py_RETURN_FALSE; }
 
 	}
 	{
 		cppy::TupleRef args(args_);
-		std::cout << "------------------------------" << std::endl
-		          << "append" << std::endl;
+		std::cout << "    ------------------------------" << std::endl
+		          << "    append" << std::endl;
 		cppy::List lst;
 		if (lst.size() != 0) { Py_RETURN_FALSE; }
-		std::cout << "before append." << std::endl;
+		std::cout << "    before append." << std::endl;
 
 		cppy::PyObjectConverter<cppy::Object> cvt;
 		PyObject* ptr = cvt(args.getitem(0));
 		if (ptr != args.getitem(0).obj) {
-			std::cout << "Convert to ptr (getitem) fail." << std::endl;
+			std::cout << "    Convert to ptr (getitem) fail." << std::endl;
 			Py_RETURN_FALSE;
 		}
 		if (ptr != (*args[0]).obj) {
-			std::cout << "Convert to ptr (*[]) fail." << std::endl;
+			std::cout << "    Convert to ptr (*[]) fail." << std::endl;
 			Py_RETURN_FALSE;
 		}
 		if (ptr != args[0]->obj) {
-			std::cout << "Convert to ptr ([]->) fail." << std::endl;
+			std::cout << "    Convert to ptr ([]->) fail." << std::endl;
 			Py_RETURN_FALSE;
 		}
 		if (ptr != cvt(args[0])) {
-			std::cout << "Convert to ptr converter([]) fail." << std::endl;
+			std::cout << "    Convert to ptr converter([]) fail." << std::endl;
 			Py_RETURN_FALSE;
 		}
 
@@ -156,43 +104,102 @@ PyObject* basic_test(PyObject *m, PyObject *args_)
 
 		lst.append(0);
 		if (lst[lst[lst.size()-1]]->obj != args[0]->obj) {
-			std::cout << "lst[lst[-1]] != lst[0]" << std::endl;
+			std::cout << "    lst[lst[-1]] != lst[0]" << std::endl;
 			Py_RETURN_FALSE;
 		}
+	}
+
+	Py_RETURN_TRUE;
+}
+
+
+PyObject* test_object(PyObject *m, PyObject *args_) {
+	{
+		std::cout << "    ------------------------------" << std::endl
+		          << "    Running basic test as generic borrowed reference object." << std::endl;
+		cppy::Object<> args(args_);
+		std::cout << "      size: " << args.size() << std::endl;
+		std::cout << "      str : " << args.str() << std::endl;
+		std::cout << "      repr: " << args.repr() << std::endl;
+	}
+
+	{
+		std::cout << "    ------------------------------" << std::endl
+		          << "    Running basic test as generic owned reference object." << std::endl;
+		cppy::Object<> borrowed(args_);
+		cppy::Object<PyObject> owned(borrowed);
+		std::cout << "      size: " << owned.size() << std::endl;
+		std::cout << "      str : " << owned.str() << std::endl;
+		std::cout << "      repr: " << owned.repr() << std::endl;
+	}
+
+	{
+		std::cout << "    ------------------------------" << std::endl
+		          << "    indexing const generic borrowed reference." << std::endl;
+		const cppy::Object<> args(args_);
+		for (int i=0; i<args.size(); ++i)
+		{ std::cout << "        " << i << ": " << args[i]->str() << std::endl; }
+	}
+
+
+	{
+		std::cout << "    ------------------------------" << std::endl
+		          << "    indexing non-const generic borrowed reference." << std::endl;
+		cppy::Object<> args(args_);
+		for (int i=0; i<args.size(); ++i)
+		{ std::cout << "        " << i << ": " << args[i]->str() << std::endl; }
 	}
 	Py_RETURN_TRUE;
 }
 
-//PyObject* tupassign_test(PyObject *m, PyObject *args_)
-//{
-//	cppy::Tuple<true> ret(
-//		1, "hello world!", 1.5, 3.25f, 5,
-//		cppy::Tuple<true>("hello", 42));
+PyObject* test_tuple(PyObject *m, PyObject *args_)
+{
+	{
+		std::cout << "    ------------------------------" << std::endl
+		          << "    using a borrowed Tuple" << std::endl;
+		cppy::TupleRef args(args_);
+		for (int i=0; i<args.size(); ++i)
+		{ std::cout << "        " << i << ": " << args[i]->str() << std::endl; }
+	}
 
-//	return ret.ret();
-//}
+	{
+		std::cout << "    ------------------------------" << std::endl
+		          << "    testing tuple creation" << std::endl;
+		cppy::TupleRef args(args_);
+		if (args.size() >= 3) {
+			cppy::Tuple tmp(
+				args[0]->obj,
+				args[1]->obj,
+				args[2]->obj
+			);
+			std::cout << "      " << tmp.str() << std::endl;
+		}
+		else {
+			cppy::Tuple tmp(1, 2, 3);
+			std::cout << "      " << tmp.str() << std::endl;
+		}
+	}
 
-//PyObject* call_test(PyObject *m, PyObject *args)
-//{
-//	int result = cppy::callcpp(
-//		[](int a, int b){ return a + b; },
-//		args);
-//	return cppy::Object<int,true>(result).ret();
-//}
+	{
+		std::cout << "    ------------------------------" << std::endl
+		          << "    tuple size_+slice" << std::endl;
+		cppy::TupleRef args(args_);
+		if (args.check()) {
+			std::cout << "      " << args.slice(1,args.size_()).str() << std::endl;
+		}
+	}
 
-//PyObject* call_test2(PyObject *m, PyObject *args_)
-//{
-//	cppy::Tuple<> args(args_);
-//	return cppy::callpy(args[0]().obj, 1, 2);
-//}
+	Py_RETURN_TRUE;
+}
+
 
 PyMODINIT_FUNC PyInit_testmodule()
 {
 	static PyMethodDef methods[] = {
-		{ "basic_test", basic_test, METH_VARARGS, "print arguments." },
-		//{ "tupassign_test", tupassign_test, METH_VARARGS, "return a tup." },
-		//{ "call_test", call_test, METH_VARARGS, "Call a c++ lambda using python arguments." },
-		//{ "call_test2", call_test2, METH_VARARGS, "Call a Python func using c++ arguments (1, 2)." },
+		{"test_object", test_object, METH_VARARGS, "Test object interface." },
+		{"test_tuple", test_tuple, METH_VARARGS, "Test tuples." },
+		{"test_convert", test_convert, METH_VARARGS, "test some conversion operators."},
+		{"test_list", test_list, METH_VARARGS, "Test list."},
 		{}
 	};
 
--- test/tst/t.py
+++ test/tst/t.py
@@ -1,37 +1,40 @@
 from tst import testmodule
 
 import sys
-errored = False
+def check(func, *args):
+    """Call func with args.
 
-tup = ([slice(10,30)],(3.14, 101), "hello", None)
-tupcount1 = sys.getrefcount(tup)
-print('arguments:', tup)
-counts1 = [sys.getrefcount(thing) for thing in tup]
-errored = not testmodule.basic_test(*tup)
-counts2 = [sys.getrefcount(thing) for thing in tup]
-if counts1 != counts2:
-    print('-------------------------')
-    print('ERROR: arg refcounts do not match.')
-    print('-------------------------')
-    print('before', counts1)
-    print('after ', counts2)
-    errored = True
-tupcount2 = sys.getrefcount(tup)
-if tupcount1 != tupcount2:
-    print('-------------------------')
-    print('ERROR: argtup refcounts do not match.')
-    print('-------------------------')
-    print('before', tupcount1)
-    print('after ', tupcount2)
-    errored = True
+    Return True if an error happened.  Otherwise, False.
+    """
+    print('------------------------------')
+    print(func.__name__)
+    print('------------------------------')
+    print('  args:', args)
+    tupcount1 = sys.getrefcount(args)
+    counts1 = list(map(sys.getrefcount, args))
+    errored = not func(*args)
+    counts2 = list(map(sys.getrefcount, args))
+    tupcount2 = sys.getrefcount(args)
+    if counts1 != counts2:
+        print('  -------------------------')
+        print('  ERROR: arg refcounts do not match.')
+        print('  -------------------------')
+        print('  before', counts1)
+        print('  after ', counts2)
+        ret = True
+    if tupcount2 != tupcount1:
+        print('  -------------------------')
+        print('  ERROR: argtup refcounts do not match.')
+        print('  -------------------------')
+        print('  before', tupcount1)
+        print('  after ', tupcount2)
+        ret = True
+    return errored
 
-
-
-
-# print('call cpp with py tup args:', 'pass' if testmodule.call_test(1, 2) == 3 else 'fail')
-# print('call py with cpp args: ', 'pass' if testmodule.call_test2((lambda x, y: x+y)) == 3 else 'fail')
-
-# print('tup creation:', 'pass' if testmodule.tupassign_test() == (1,'hello world!',1.5,3.25,5, ('hello', 42)) else 'fail')
-# print(testmodule.tupassign_test())
-if errored:
+if (
+    check(testmodule.test_object, [slice(10,30)], (3.14, 101), "hello", None)
+    or check(testmodule.test_tuple, [slice(10,30)], (3.14, 101), "hello", None)
+    or check(testmodule.test_convert, 1,2,3,'a', 'b', 'c')
+    or check(testmodule.test_list, 'abc', 123)
+):
     sys.exit(1)
