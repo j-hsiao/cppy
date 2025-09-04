8f8f0c2d4a290851bced050de25f040b2058bbb0
Jason Hsiao <oaishnosaj@gmail.com>
2025-09-04 21:47:34 +0800

    expanding module implementation.

--- include/cppy/module.hpp
+++ include/cppy/module.hpp
@@ -1,14 +1,49 @@
-
+#ifndef CPPY_MODULE_HPP
+#define CPPY_MODULE_HPP
+#include <cppy/object.hpp>
 namespace cppy
 {
 
-	struct PyModule
-	{
-		const char *name;
+	template<class T> struct statesize { static constexpr Py_ssize_t size = static_cast<Py_ssize_t>(sizeof(T)); };
+	template<> struct statesize<void> { static constexpr Py_ssize_t size = -1; };
+
+	struct Module {
+		struct PyModuleDef mdef;
+		PyObject md;
+
+		template<
+			class State=void,
+			class Methods=PyMethodDef*,
+			class Slots=PyModuleDef_Slot*
+		>
+		Module(
+			const char *name,
+			State state,
+			Methods methods=nullptr,
+			Slots slots=nullptr,
+			traverseproc=nullptr,
+			inquiry=nullptr,
+			freefunc=nullptr
+		):
+			mdef{
+				PyModuleDef_HEAD_INIT,
+				name,
+				docs,
+				statesize<State>::size,
+				methods,
+				slots,
+				traverse,
+				clear,
+				free
+			},
+			md(PyModule_Create(&mdef))
+		{
+			//TODO multistage initialization?
+		}
 
 
 
-		struct PyModuleDef ()
 	};
 
 }
+#endif//CPPY_MODULE_HPP
